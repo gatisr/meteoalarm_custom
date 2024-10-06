56c7f9b103f51cfc68071292e4ec2c2e9161c874
Gatis Rīders <gatis.riders@gmail.com>
2024-10-06 14:23:42 +0300

    fix init (#1)

--- custom_components/meteoalarm_custom/__init__.py
+++ custom_components/meteoalarm_custom/__init__.py
@@ -1,5 +1,6 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN, PLATFORMS
 
@@ -7,7 +8,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    try:
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    except Exception as exc:
+        raise ConfigEntryNotReady from exc
 
     return True
 
